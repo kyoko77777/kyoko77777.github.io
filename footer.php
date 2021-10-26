f9c68967ba980edb7825ddfc07bdfef443ea8aac
kyoko77777 <84776403+kyoko77777@users.noreply.github.com>
2021-10-26 15:02:27 +0900

    211026_作成

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="site-foot" role="contentinfo">
+	<div class="footer-inner">
+		<div class="footer-logo">
+			<img src="/images/footer-logo.svg" alt="">
+		</div>
+		<!-- /.footer-logo -->
+		<div class="footer-address">
+			<p><strong>クアハウス山小屋</strong></p>
+			<p>〒259-1101 神奈川県伊勢原市日向2184-1<br>
+			TEL 0463-92-7750 / FAX 0463-92-7760<br>
+			定休日：4〜10月 無休 / 11〜3月 平日火曜 ※1月1〜5日・祭日は営業</p>
+
+			<p><strong>日向渓谷マス釣り場</strong></p>
+			<p>〒259-1101 神奈川県伊勢原市日向1770<br>
+			TEL 0463-94-7220 / FAX 0463-94-7220<br>
+			定休日：4〜10月 無休 / 11〜3月 土日祭日のみ営業 平日は休業<br>
+			営業時間：8:00〜17:00</p>
+		</div>
+		<!-- /.footer-address -->
+	</div>
+	<!-- /.footer-inner -->
+	<div class="footer-navi">
+		<ul>
+			<li><a href="/camp">キャンプ</a></li>
+			<li><a href="/bbq">バーベキュー</a></li>
+			<li><a href="/bath">お風呂・レストラン</a></li>
+			<li><a href="/fishing">日向渓谷マス釣り場</a></li>
+			<li><a href="/reserve">ご予約</a></li>
+			<li><a href="/access">アクセス</a></li>
+			<li><a href="/faq">よくあるご質問</a></li>
+			<li><a href="/movie">動画</a></li>
+			<li><a href="/usage">ご利用にあたって</a></li>
+		</ul>
+	</div>
+	<!-- /.footer-navi -->
+
+<div class="copyright">
+  <small>Copyright &copy;Kyoko  All Rights Reserved.</small>
+</div>
+<!-- /.copyright -->
+  </footer>
---
+++ header.php
@@ -0,0 +1,49 @@
+<header class="site-head" role="banner">
+	<div class="header-inner">
+		<div class="logo">
+			<h1><a href="/"><img src="/images/logo.svg" alt="logo" width="160" height="65"></a></h1>
+		</div>
+		<!-- /.logo -->
+		<nav class="site-navi" role="navigation">
+			<ul>
+				<li class="navi-port"><a href="/portforio">Portforio</a></li>
+				<li class="navi-about"><a href="/about">About</a></li>
+				<li class="navi-contact"><a href="/contact">>Contact</a></li>
+			</ul>
+		</nav>
+		<!-- /.site-navi -->
+	</div>
+	<!-- /.header-inner -->
+</header>
+<!-- /.site-head -->
+
+<div class="sp-head">
+	<div class="logo">
+		<a href="/"><img src="/images/logo.svg" alt="logo"></a>
+	</div>
+	<!-- /.logo -->
+</div>
+<!-- /.sp-head -->
+
+<div class="sp-tgl">
+	<span></span>
+	<span></span>
+	<span></span>
+	<p>MENU</p>
+</div>
+<!-- /.sp-tgl -->
+<div class="sp-navi">
+	<nav role="navigation">
+		<div class="logo">
+			<h1><img src="/images/logo.png" alt="logo" width="160" height="65"></h1>
+		</div>
+		<!-- /.logo -->
+		<ul>
+			<li><a href="/portforio">Portforio</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/contact">>Contact</a></li>
+		</ul>
+	</nav>
+	<!-- /.site-navi -->
+</div>
+<!-- /.sp-navi -->
